5687f00ea7a2fe3dafbfac79a56c4cfe6c514151
Mike <63655075+MixelTe@users.noreply.github.com>
2025-02-07 19:18:27 +0300

    implement logic

---
+++ ts/functions.ts
@@ -0,0 +1,22 @@
+export function getButton(id: string)
+{
+	return getEl(id, HTMLButtonElement);
+}
+export function getHeading(id: string)
+{
+	return getEl(id, HTMLHeadingElement);
+}
+export function getEl<T extends typeof HTMLElement>(id: string, type: T)
+{
+	const el = <unknown | null>document.getElementById(id);
+	if (el == null) throw new Error(`${id} not found`);
+	if (el instanceof type) return el as InstanceType<T>;
+	throw new Error(`${id} element not ${type.name}`);
+}
+
+export function addButtonListener(id: string, f: (e: MouseEvent, btn: HTMLButtonElement) => void)
+{
+	const button = getButton(id);
+	button.addEventListener("click", e => f(e, button));
+	return button;
+}
---
+++ ts/main.ts
@@ -0,0 +1,180 @@
+import { addButtonListener, getHeading } from "./functions.js";
+
+const stateEl = getHeading("state");
+const resultEl = getHeading("result");
+
+addButtonListener("btnC", () =>
+{
+	state = getCleanState();
+	saveState();
+	updateUI();
+});
+addButtonListener("btnCE", () =>
+{
+	state.input = "0";
+	saveState();
+	updateUI();
+});
+addButtonListener("btnDel", () =>
+{
+	if (state.input.length <= 1) state.input = "0";
+	else state.input = state.input.slice(0, state.input.length - 1);
+	saveState();
+	updateUI();
+});
+addButtonListener("btn1", () => digit(1));
+addButtonListener("btn2", () => digit(2));
+addButtonListener("btn3", () => digit(3));
+addButtonListener("btn4", () => digit(4));
+addButtonListener("btn5", () => digit(5));
+addButtonListener("btn6", () => digit(6));
+addButtonListener("btn7", () => digit(7));
+addButtonListener("btn8", () => digit(8));
+addButtonListener("btn9", () => digit(9));
+addButtonListener("btn0", () => digit(0));
+addButtonListener("btn,", () =>
+{
+	if (!state.input.includes(","))
+		state.input = `${state.input},`;
+	saveState();
+	updateUI();
+});
+addButtonListener("btnSign", () =>
+{
+	if (state.input.startsWith("-"))
+		state.input = state.input.slice(1);
+	else
+		state.input = "-" + state.input;
+	saveState();
+	updateUI();
+});
+addButtonListener("btnInv", () => unary(x => 1 / x));
+addButtonListener("btnSq", () => unary(x => x * x));
+addButtonListener("btnSqrt", () => unary(Math.sqrt));
+addButtonListener("btn/", () => operation("/"));
+addButtonListener("btn*", () => operation("*"));
+addButtonListener("btn-", () => operation("-"));
+addButtonListener("btn+", () => operation("+"));
+addButtonListener("btn=", calc);
+addButtonListener("btn%", () =>
+{
+	if (state.saved != null && state.operation != "" && state.saved2 == null)
+	{
+		if (state.operation == "+" || state.operation == "-")
+			state.input = `${getNum(state.saved) / 100 * getNum(state.input)}`;
+		else
+			state.input = `${getNum(state.input) / 100}`;
+		saveState();
+		updateUI();
+	}
+});
+
+let state = loadState();
+updateUI();
+
+
+
+function digit(v: number)
+{
+	if (state.input == "0" || state.clearInputOnType) state.input = `${v}`;
+	else state.input = `${state.input}${v}`;
+	state.clearInputOnType = false;
+	saveState();
+	updateUI();
+}
+function operation(v: Operation)
+{
+	if (!state.clearInputOnType) calc();
+	state.saved = state.input;
+	state.saved2 = null;
+	state.operation = v;
+	state.clearInputOnType = true;
+	saveState();
+	updateUI();
+}
+function unary(calcF: (x: number) => number)
+{
+	state.input = `${calcF(getNum(state.input))}`;
+	if (state.saved2 != null && state.clearInputOnType)
+	{
+		state.operation = "";
+		state.saved2 = null;
+		state.saved = null;
+	}
+	state.clearInputOnType = true;
+	saveState();
+	updateUI();
+}
+
+function calc()
+{
+	if (state.saved == null || state.operation == "") return;
+	if (state.saved2 != null && state.clearInputOnType)
+		state.saved = state.input;
+	else
+		state.saved2 = state.input;
+	state.input = `${{
+		"+": (a: number, b: number) => a + b,
+		"-": (a: number, b: number) => a - b,
+		"/": (a: number, b: number) => a / b,
+		"*": (a: number, b: number) => a * b,
+	}[state.operation](getNum(state.saved), getNum(state.saved2))}`.replace(".", ",");
+	state.clearInputOnType = true;
+	saveState();
+	updateUI();
+}
+function getNum(num: string | null)
+{
+	if (num === null) return 0;
+	return parseFloat(num.replace(",", "."))
+}
+
+function updateUI()
+{
+	if (state.saved === null) stateEl.innerText = "";
+	else if (state.operation != "")
+	{
+		stateEl.innerText = state.saved + " " + {
+			"+": "+",
+			"-": "-",
+			"/": "÷",
+			"*": "×",
+		}[state.operation];
+		if (state.saved2 !== null)
+			stateEl.innerText += " " + state.saved2 + " =";
+	}
+	else stateEl.innerText = state.saved;
+	resultEl.innerText = state.input;
+}
+
+
+function loadState()
+{
+	const stateStr = localStorage.getItem("calculator_state");
+	if (stateStr)
+		return JSON.parse(stateStr) as State;
+	return getCleanState();
+}
+function saveState()
+{
+	localStorage.setItem("calculator_state", JSON.stringify(state));
+}
+function getCleanState()
+{
+	return {
+		input: "0",
+		clearInputOnType: false,
+		saved: null,
+		saved2: null,
+		operation: "",
+	} as State
+}
+interface State
+{
+	input: string;
+	clearInputOnType: boolean
+	saved: string | null;
+	saved2: string | null;
+	operation: Operation;
+}
+type Operation = "" | "+" | "-" | "/" | "*"
